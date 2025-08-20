0ac73f1e684a570aceb62125ae93af8bef6a56e8
moetezjebari <moetez.jebari@esprit.tn>
2025-08-20 15:55:42 +0100

    second

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Service.UserService;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
      
     private final UserService userService;
